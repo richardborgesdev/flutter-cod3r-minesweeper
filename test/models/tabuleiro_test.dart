03fa6986b2094b1974e754e19c2400ff9bd926ba
Richard Borges <richard.borges.cc@gmail.com>
2023-06-26 10:35:34 -0300

    chore: tabuleiro test

---
+++ test/models/tabuleiro_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_cod3r_minesweeper/models/tabuleiro.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  test('Ganhar jogo', () {
+    Tabuleiro tabuleiro = Tabuleiro(
+      linhas: 2,
+      colunas: 2,
+      qtdeBombas: 0,
+    );
+
+    tabuleiro.campos[0].minar();
+    tabuleiro.campos[3].minar();
+
+    tabuleiro.campos[0].alternarMarcacao();
+    tabuleiro.campos[1].abrir();
+    tabuleiro.campos[2].abrir();
+    tabuleiro.campos[3].alternarMarcacao();
+
+    expect(tabuleiro.resolvido, isTrue);
+  });
+}
